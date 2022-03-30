ecf2771fe0e6af99d7f5b2f6c7d0a7e736c077d2
sergey.lebidko <sergeyler@mail.ru>
2022-03-30 10:13:50 +0300

    Настроен ESLINT

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      Wilix Search
-    </div>
-  );
-}
+  return <div>Wilix Search</div>;
+};
 
 export default App;
