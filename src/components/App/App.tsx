04f60f753497555d864e60a178cdd7db4289bfc2
sergey.lebidko <sergeyler@mail.ru>
2022-03-30 12:41:59 +0300

    Завершена верстка для строки поиска

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 const App: React.FC = () => {
+  const clickHandler = (): void => console.log('Сработало');
+
   return (
-    <div>
-      <h1>Wilix Search</h1>
-      <input placeholder="Введите запрос"/>
-    </div>
+    <Container>
+      <Stack gap={2}>
+        <h1 className="text-center">Wilix Search</h1>
+        <Stack gap={2} direction="horizontal">
+          <Form.Control type="text" placeholder="Введите запрос"/>
+          <Button variant="primary" onClick={clickHandler}>Найти</Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
 
