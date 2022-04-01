df57ae6f4c415224eb0e8f8de7aece0f3f6302e2
sergey.lebidko <sergeyler@mail.ru>
2022-04-01 09:21:11 +0300

    Удалены неиспользуемые компоненты

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,6 @@
 import React, {useEffect} from 'react';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 import SearchControl from '../SearchControl/SearchControl';
-import Post from '../result_components/Post/Post';
-import Phone from '../result_components/Phone/Phone';
-import Nick from '../result_components/Nick/Nick';
-import FamilyName from '../result_components/FamilyName/FamilyName';
-import IPAddress from '../result_components/IPAddress/IPAddress';
 import {useSelector} from 'react-redux';
 import {resultSelector} from '../../store/selectors';
 import {
@@ -17,13 +12,14 @@ import {
   POST_URL,
 } from '../../settings';
 import Error from '../result_components/Error/Error';
+import ResultCard from '../ResultCard/ResultCard';
 
 const App: React.FC = () => {
   const navigate = useNavigate();
   const result = useSelector(resultSelector);
 
   useEffect(() => {
-    // Если никакие запросы еще не выполнялись - переводим пользователя на пустую страницу
+    // Если никаких результатов парсинга в хранилище нет - переводим пользователя на пустую главную страницу
     if (result === null) {
       navigate('/');
       return;
@@ -37,11 +33,11 @@ const App: React.FC = () => {
     <Routes>
       <Route path="/" element={<SearchControl/>}>
         <Route path={ERROR_URL} element={<Error/>}/>
-        <Route path={POST_URL} element={<Post/>}/>
-        <Route path={PHONE_URL} element={<Phone/>}/>
-        <Route path={NICK_URL} element={<Nick/>}/>
-        <Route path={FAMILY_NAME_URL} element={<FamilyName/>}/>
-        <Route path={IP_ADDRESS_URL} element={<IPAddress/>}/>
+        <Route path={POST_URL} element={<ResultCard headerText="Адрес электронной почты"/>}/>
+        <Route path={PHONE_URL} element={<ResultCard headerText="Номер телефона"/>}/>
+        <Route path={NICK_URL} element={<ResultCard headerText="Никнэйм"/>}/>
+        <Route path={FAMILY_NAME_URL} element={<ResultCard headerText="Имя и фамилия"/>}/>
+        <Route path={IP_ADDRESS_URL} element={<ResultCard headerText="IP-адрес"/>}/>
       </Route>
     </Routes>
   );
